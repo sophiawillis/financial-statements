1272516fab98abfd31fc52c68ebfb42e9f1a508e
swillis <32820862+sophiawillis@users.noreply.github.com>
2025-10-06 22:05:36 -0400

    Update finance_statements.py

--- finance_statements.py
+++ finance_statements.py
@@ -1,10 +1,11 @@
 #making function that allows you to plug in any ticker and get the three statements saved as excel
 #note this is quarterly data, if you don't want that use 'financials.T' instead of 'quarterly_financials.T'
+#default ticker is google
 
 import yfinance as yf
 import pandas as pd
 
-def fin_statements(ticker):
+def fin_statements(ticker, ticker = "GOOG"):
     company = yf.Ticker(ticker)
     income = company.quarterly_financials.T
     balance = company.quarterly_balance_sheet.T
